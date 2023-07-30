ab190a09a969438a1e623050bf1ce1ef1f443a64
ironhiro <iuncehiro@gmail.com>
2023-07-30 14:37:52 +0900

    API Key 숨김 처리 및 위치 알아내는 Test 함수 제작

--- WhereIsIt/MainPage.xaml.cs
+++ WhereIsIt/MainPage.xaml.cs
@@ -1,12 +1,31 @@
 ﻿
-using System;namespace WhereIsIt;
+using Microsoft.Maui.Maps;
+using System;
+using WhereIsIt.ViewModel;
+
+namespace WhereIsIt;
 
 public partial class MainPage : ContentPage
-{    public void SetCurrentLocation(object sender, EventArgs e)
+{    public async void SetCurrentLocation(object sender, EventArgs e)
     {
 		map.IsShowingUser = true;
+		var dataContext = this.BindingContext as TestViewModel;
+		var location = await dataContext.GetCurrentLocation();
+		if(location != null)
+		{
+			map.MoveToRegion(MapSpan.FromCenterAndRadius(location, Distance.FromKilometers(3)));
+			map.Pins.Add(new Microsoft.Maui.Controls.Maps.Pin
+			{
+				Label = "현재 위치",
+				Location = location,
+				Type = Microsoft.Maui.Controls.Maps.PinType.Generic
+			});
+		}
+		else
+		{
+			await DisplayAlert("Error", "There is Error at saving Location", "OK");
+		}
     }
-
 
 	public MainPage()
 	{
